2208463a9194fdd8c425a8e7383647d78c1751ca
mohanMERNdev <mohanjayabal5@gmail.com>
2024-08-27 11:47:54 +0530

    port

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ app.get('/cities', (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
